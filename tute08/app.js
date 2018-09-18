cebf628faa4980292ce9e3d37c6279d13667be40
MD Ariful Islam <ariful.islam@stud.uni-due.de>
2018-09-18 22:55:44 +0200

    image file upload using multer

--- tute08/app.js
+++ tute08/app.js
@@ -6,31 +6,43 @@ const app = express();
 const exphbs  = require('express-handlebars');
 const path = require("path");
 
+//nodemailer includes
+const nodemailer = require("nodemailer");
+
 //file system
 const fs = require('fs');
 var LocalStorage = require('node-localstorage').LocalStorage;
 localStorage = new LocalStorage('./userData');
 
 const multer = require('multer');
-// setting storage
+// multer setting storage
 const storage = multer.diskStorage({
     destination: function (req, file, callback) {
-        callback(null, 'upload/')
+        callback(null, 'public/upload/images')
     },
     filename: function (req, file, callback) {
-        callback(null, file.originalname + Date.now() + path.extname(file.originalname))
-    }
+        callback(null, Date.now() + file.originalname)
+    },
+/*    //A means of ensuring only images are uploaded.
+    fileFilter: function(req, file, next){
+        if(!file){
+            next();
+        }
+        const image = file.mimetype.startsWith('image/');
+        if(image){
+            console.log('photo uploaded');
+            next(null, true);
+        }else{
+            console.log("file not supported");
+            return next();
+        }
+    }*/
 });
 
 const upload = multer({ storage: storage });
-/*
-const upload = multer({
-    dest: 'upload/',
-    filename: function (req, file, callback) {
-        callback(null, file.originalname);
-    }
-});
-*/
+
+// set uploads folder
+app.use(express.static(__dirname + '/public'));
 
 // This is config for render view in `views` folder
 // and use handlebars as template engine here
@@ -88,52 +100,69 @@ app.get('/home', (req, res)=> {
     res.render('home')
 });
 
-// file upload
-/*app.post('/upload', upload.single('my-pic'), (req, res) => {
+// file upload routes
+app.post('/upload', upload.single('my-pic'), (req, res) => {
     // get temporary file path or destination
     const tempPath = req.file.path;
+    // that has to add localstorage or database as profile image
+
     console.log(tempPath) // check where file uploads
 
-    // target path to upload with full path of server or localstorage
-    const targetPath = path.join(__dirname, "upload/images");
-    console.log(targetPath)
+/*    // target path to upload with full path of server or localstorage
+    const targetPath = path.join(__dirname, "public/upload/images");
+    console.log(targetPath)*/
 
     //check uploaded file original name
-    console.log(req.file.originalname)
+    //console.log(req.file.originalname)
 
     // check file extension name
     const uploadedFileExt = path.extname(req.file.originalname).toLowerCase();
-    console.log(uploadedFileExt);
+    // console.log(uploadedFileExt);
 
     // rename only jpg file else delete from temporary uploads/
-    if (uploadedFileExt === '.jpg') {
-        //rename file name using target path
-        fs.rename(tempPath, targetPath, (err, res)=> {
-            if (err) throw err;
-            res.status(200);
-            res.contentType("text/plain")
-            res.end("File uploaded successfully........");
-        });
+    if ((uploadedFileExt !== '.jpg') || (uploadedFileExt !== '.png')) {
+        res.redirect('/profile/' + tempPath);
     } else {
+        // if not jpg remove file and give a message
         fs.unlink(tempPath, err => {
             if (err) throw err;
             res.status(403);
             res.contentType("text/plain")
-            res.end("Please upload only JPG file");
+            res.end("Please upload only JPG or PNG file");
         });
     }
-
-    res.redirect('/profile');
-});*/
-
-
-app.post('/upload', upload.single('my-pic'), function (req, res) {
-   res.redirect('/profile');
-})
+});
 
 // profile route
-app.get('/profile', (req, res)=> {
-    res.render('profile')
+app.get('/profile/:image', (req, res)=> {
+    var tempPath =  req.params.image;
+    var imagePath = tempPath.replace(/public/i, '');
+    console.log(imagePath);
+    res.render('profile', { imagePath: imagePath });
+});
+
+// setup transport service for your email service
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'test.purpose.mai@gmail.com',
+        pass: '@r1ful2018'
+    }
+});
+
+//configure email details
+const mailOptions = {
+    from: 'test.purpose.mai@gmail.com', // sender address
+    to: 'mac.fira@gmail.com', // list of receivers
+    subject: 'Nodemailer test Ariful', // Subject line
+    html: '<p>Hello ariful....</p>'// plain text body
+};
+
+transporter.sendMail(mailOptions, function (err, info) {
+    if(err)
+        console.log(err)
+    else
+        console.log(info);
 });
 
 app.listen(3000, () => console.info('Application running on port 3000'));
