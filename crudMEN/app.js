56a7f8cf31c988bd22bede45021eb67464dd9177
ariffira <mac.fira@gmail.com>
2018-09-30 20:08:38 +0200

    test2

--- crudMEN/app.js
+++ crudMEN/app.js
@@ -1,5 +1,5 @@
 var createError = require('http-errors');
-var express = require('express');
+const express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
